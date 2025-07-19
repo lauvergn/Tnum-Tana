54338ebf3c3c2828a6bb8b93af855454a372c2f4
lauvergn <David.Lauvergnat@universite-paris-saclay.fr>
2025-07-19 09:36:07 +0200

    change cleanlib and get_AllLibs.sh

--- Ext_Lib/get_AllLibs.sh
+++ Ext_Lib/get_AllLibs.sh
@@ -4,7 +4,7 @@ dev=$1
 
 for lib in QDUtilLib AD_dnSVM ConstPhys QuantumModelLib nDindex EVRT_dnSVM FOR_EVRT
 do
-  rm -f $lib # remove the link
+  rm -fr $lib # remove the link
   rm -rf $lib* # remove the directory
   get_lib.sh $lib $dev
 done
